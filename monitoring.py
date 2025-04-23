45a5d50f03dbeeb9a5d92cbfee9ed13b95050411
D-973 <nanlohy.dhea@gmail.com>
2025-04-23 20:24:18 +0800

    Test 1

---
+++ monitoring.py
@@ -0,0 +1,36 @@
+import google.generativeai as genai
+import datetime
+import requests
+import subprocess
+
+# Konfigurasi API Gemini
+genai.configure(api_key="AIzaSyDSILjc0WLk3FpLdpkmWvEbt1hZnuCQeo0")
+
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
+def get_ssh_attempts():
+    result = subprocess.check_output("grep 'Failed password' /var/log/auth.log | tail -n 10", shell=True)
+    return result.decode()
+
+def get_gemini_analysis(log_text):
+    try:
+        response = model.generate_content(f"Ada percobaan login brute force:\n{log_text}\nApa yang sebaiknya saya lakukan?. responnya jangan terlalu panjang")
+        return response.text
+    except Exception as e:
+        return f"⚠️ Gagal mendapatkan analisis dari Gemini: {e}"
+
+def send_whatsapp(message):
+    token = "aSJ8LTKFEQz2GG8L1SYN"
+    payload = {
+        "target": "nomor hp yang akan digunakan",
+        "message": message,
+    }
+    headers = {"Authorization": token}
+    r = requests.post("https://api.fonnte.com/send", data=payload, headers=headers)
+    return r.status_code
+
+# Eksekusi semua
+log = get_ssh_attempts()
+ai_response = get_gemini_analysis(log)
+full_message = f"[{datetime.datetime.now()}] ⚠️ Percobaan Login Detected!\n\n{log}\n\n🧠 Gemini says:\n{ai_response}"
+send_whatsapp(full_message)
